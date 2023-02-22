d6e462c05b6f30c7f5dc08ba2be6f6210c9dbc3b
Leonardo Coltri <leocoltri@outlook.com>
2023-02-22 04:05:39 +0000

    Terraform files

---
+++ terraform/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "gcs" {
+      bucket  = "finance-tf-state"
+      prefix  = "terraform/state"
+  }
+
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  
+}
+
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,2 @@
+project_id = "finance-project-378602"
+region     = "us-central1"
